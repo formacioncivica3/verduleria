a6f8c88cba71b7dee868609a857c0ddb32761feb
formacioncivica3 <79419192+formacioncivica3@users.noreply.github.com>
2021-12-03 19:53:29 -0600

    Add files via upload

---
+++ webstore-feature-01-MaquetaTuTiendaOnline/poject files - Copy/js/buscador.js
@@ -0,0 +1,21 @@
+var consulta = $("#searchTable").DataTable();
+
+$("#inputBusqueda").keyup(function(){
+	consulta.search($(this).val()).draw();
+
+	$("header").css({
+		"height": "30vh",
+	})
+
+	if ($("#inputBusqueda").val() == ""){
+		$("header").css({
+			"height": "auto",
+			"background": "none"
+		})
+
+		$("#search").hide()
+
+	} else {
+		$("#search").fadeIn("fast");
+	}
+})
